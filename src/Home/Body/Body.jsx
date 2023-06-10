cca73dffbc104c9805942e5cc136ce57df3ba047
Md Saidul Islam <saiduli066@gmail.comm>
2023-06-11 01:28:24 +0600

    popular teachers section is added

--- src/Home/Body/Body.jsx
+++ src/Home/Body/Body.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable no-unused-vars */
 import React, { useEffect, useState } from 'react';
-import { FaBookOpen, FaLaptop, FaLockOpen, FaUsers } from 'react-icons/fa';
+import { FaBookOpen, FaLaptop, FaLockOpen, FaRegStar, FaStar, FaUsers } from 'react-icons/fa';
+import Rating from 'react-rating';
 
 const Body = () => {
 
@@ -9,10 +10,19 @@ const Body = () => {
         fetch("https://summer-camp-server-side-seven.vercel.app/popularClasses")
         .then(res=>res.json())
         .then(data=>setPopularClasses(data))
+    }, [])
+    
+    const [popularTeachers, setPopularTeachers] = useState([]);
+    useEffect(() => {
+        fetch("https://summer-camp-server-side-seven.vercel.app/popularTeachers")
+        .then(res=>res.json())
+        .then(data=>setPopularTeachers(data))
     },[])
+
+
     return (
-      <div className="my-4 md:my-8 ">
-        <div className="grid md:grid-cols-4 grid-cols-2 justify-items-center my-4 px-4 md:my-8">
+      <div className="my-4 md:my-8">
+        <div className="grid md:grid-cols-4 grid-cols-2 gap-5 justify-items-center my-4 px-4 md:my-8">
           <div className="flex items-center gap-3">
             <FaLaptop className="text-2xl text-green-400" />
             <h3 className="text-xl md:text-2xl">Online Tutoring</h3>
@@ -32,7 +42,7 @@ const Body = () => {
         </div>
 
         <div>
-          <h2 className="text-xl md:text-3xl md:font-[600] text-center">
+          <h2 className="text-xl md:text-5xl font-[600] text-center my-4 md:my-10">
             Choose your course
           </h2>
           <p className="font-normal px-4 md:px-8 py-4">
@@ -44,24 +54,71 @@ const Body = () => {
             pronunciation, listening, speaking, reading, and writing.
           </p>
         </div>
-        <h2 className="text-xl md:text-3xl md:font-[600] text-center">
+
+        {/* popular classes */}
+
+        <h2 className="text-xl md:text-5xl font-[600] text-center my-4 md:my-10">
           Popular Courses
         </h2>
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-2 md:gap-4 p-3 md:p-8">
+        <div className="w-full grid grid-cols-1 md:grid-cols-3 gap-2  md:gap-4 p-3 ">
           {popularClasses.map((popularClass) => (
             <div
               key={popularClass.id}
-              className="card w-96 bg-base-100 shadow-xl"
+              className="card w-80 md:w-96 bg-base-100 mx-auto shadow-xl"
             >
-              <figure className="px-10 pt-10">
+              <figure className="px-4 pt-4">
                 <img src={popularClass.image} alt="" className="rounded-xl" />
               </figure>
               <div className="card-body items-center text-center">
                 <h2 className="card-title">{popularClass.name} language</h2>
-                <p>{popularClass.instructor}</p>
-                <div className="card-actions">
-                  <button className="btn btn-primary">Enroll Now</button>
+                <p>
+                  <span className="font-[500]">Instructor</span>:
+                  {popularClass.instructor}
+                </p>
+
+                <div className="card-actions items-center ">
+                  <div className="flex items-center">
+                    <Rating
+                      placeholderRating={popularClass.classRatings}
+                      readonly
+                      emptySymbol={<FaRegStar />}
+                      placeholderSymbol={<FaStar className="text-orange-400" />}
+                      fullSymbol={<FaStar />}
+                    ></Rating>
+                    <span>{popularClass.classRatings}</span>
+                  </div>
+                  <div>
+                    <button className="btn btn-primary">Enroll Now</button>
+                  </div>
                 </div>
+              </div>
+            </div>
+          ))}
+        </div>
+
+        {/* popular teachers */}
+        <h2 className="text-xl md:text-5xl font-[600] text-center my-4 md:my-10">
+          Popular Teachers
+        </h2>
+        <div className="w-full grid grid-cols-1 md:grid-cols-3 gap-2  md:gap-4 p-3 ">
+          {popularTeachers.map((popularTeacher) => (
+            <div
+              key={popularTeacher.id}
+              className="card w-80 md:w-96 bg-base-100 mx-auto shadow-xl"
+            >
+              <figure className="px-4 pt-4">
+                <img
+                  src={popularTeacher.image}
+                  alt=""
+                  className="object-contain w-full h-[295px] rounded-xl"
+                />
+              </figure>
+              <div className="card-body items-center text-center">
+                <h2 className="card-title">{popularTeacher.name}</h2>
+                <p>
+                  <span className="font-[500]">Email</span>:
+                  {popularTeacher.email}
+                </p>
               </div>
             </div>
           ))}
